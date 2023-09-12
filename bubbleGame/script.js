96a459b264c82e35468bb806e8c1b5f6a3e5ebf3
kushagra_kumar <96875614+krAtOsnana@users.noreply.github.com>
2023-09-12 06:59:07 +0000

    BASICC game completeed

--- bubbleGame/script.js
+++ bubbleGame/script.js
@@ -1,3 +1,8 @@
+var timer=6;
+var newHit;
+var score=0;
+
+
 function makeBubble(){
     var temp=""
 for(var i=0;i<18*8;i++){
@@ -6,4 +11,48 @@ for(var i=0;i<18*8;i++){
 }
 document.querySelector("#bottompanel").innerHTML=temp
 }
-makeBubble()
+
+function runtimer()
+{
+    var timerint=setInterval(function(){
+            if(timer>0){
+                timer--;
+                document.querySelector("#timerval").textContent=timer
+            }
+            else{
+                clearInterval(timerint);
+                document.querySelector("#bottompanel").innerHTML=`game over <br> final score ${score}`
+
+                
+            }
+    },1000)
+
+}
+
+function getNewHit(){
+    newHit=Math.floor(Math.random()*10)
+    document.querySelector("#newHit").textContent=newHit
+}
+
+function incScore(){
+    score+=10
+    document.querySelector("#score").textContent=score
+    
+}
+
+function hitOperation(){
+   var pbtm= document.querySelector("#bottompanel");
+    pbtm.addEventListener("click",function(dets){
+      var hitBubble= Number(dets.target.textContent)
+      if(hitBubble===newHit){
+        makeBubble();
+        getNewHit();
+        incScore();
+      }
+    })
+}
+
+hitOperation();
+getNewHit();
+runtimer();
+makeBubble();
